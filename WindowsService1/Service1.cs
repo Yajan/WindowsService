cabec48156ae6f760f895cee0a7d6344adda0436
Yajana <31155354+Yajan@users.noreply.github.com>
2018-06-07 11:00:17 +0530

    Update Service1.cs

--- WindowsService1/Service1.cs
+++ WindowsService1/Service1.cs
@@ -9,6 +9,7 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Net;
 
 
 namespace WindowsService1
@@ -30,17 +31,19 @@ namespace WindowsService1
             //System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
             string[] log = { "Starting server at "+ DateTime.Now.ToString("h:mm:ss tt") };
             System.IO.File.WriteAllLines((AppDomain.CurrentDomain.BaseDirectory + "ExecutionLog.txt"), log);
-
             string output = ExecuteSetup();
-     
-            System.IO.File.AppendAllLines((AppDomain.CurrentDomain.BaseDirectory + "ExecutionLog.txt"),new []{ output});
+            System.IO.File.AppendAllLines((AppDomain.CurrentDomain.BaseDirectory + "ExecutionLog.txt"),new []{ output });
         }
 
         protected override void OnStop()
         {
             string[] log = { "Stoping server at " + DateTime.Now.ToString("h:mm:ss tt") };
+            string output = ExecuteSetup();
+            System.IO.File.AppendAllLines((AppDomain.CurrentDomain.BaseDirectory + "ExecutionLog.txt"), new[] { output });
             System.IO.File.AppendAllLines((AppDomain.CurrentDomain.BaseDirectory + "ExecutionLog.txt"),log);
         }
+
+        
 
         private string ExecuteSetup()
         {
@@ -53,14 +56,24 @@ namespace WindowsService1
             start.WindowStyle = ProcessWindowStyle.Hidden;
             start.CreateNoWindow = true;
 
-            using (Process process = Process.Start(start))
+            try
             {
-                using (StreamReader reader = process.StandardOutput)
+                using (Process process = Process.Start(start))
                 {
-                    string result = reader.ReadToEnd();
-                    return result;
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        return result;
+                    }
                 }
             }
+            catch(Exception exp)
+            {
+                Debug.Write(exp);
+                return exp.ToString();
+            }
+            
         }
+
     }
 }
