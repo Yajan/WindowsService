1c871d8f62dfb86767f15a6e838ec4d5de8f10be
Yajana <31155354+Yajan@users.noreply.github.com>
2018-06-07 10:07:22 +0530

    Add files via upload

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            //System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt");
+            string[] flag = { "Starting server at "+ DateTime.Now.ToString("h:mm:ss tt") };
+            System.IO.File.WriteAllLines((AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt"), flag);
+
+            string output = ExecuteSetup();
+     
+            System.IO.File.AppendAllLines((AppDomain.CurrentDomain.BaseDirectory + "OnStart.txt"),new []{ output});
+        }
+
+        protected override void OnStop()
+        {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "OnStop.txt");
+        }
+
+        private string ExecuteSetup()
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = ConfigurationManager.AppSettings["compiler"]; 
+            start.Arguments = ConfigurationManager.AppSettings["script"];   
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            start.RedirectStandardError = true;
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = true;
+
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+    }
+}
